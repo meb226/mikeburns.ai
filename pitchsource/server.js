7f4b9da706f9c1948b743c3a5e39550659f76873
meb226 <mikeburns226@gmail.com>
2025-12-19 19:19:50 -0500

    Remove node_modules from tracking

--- pitchsource/server.js
+++ pitchsource/server.js
@@ -16,8 +16,8 @@ const { Redis } = require('@upstash/redis');
 
 // Initialize Upstash Redis (for usage logging)
 const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+  url: process.env.KV_REST_API_URL,
+  token: process.env.KV_REST_API_TOKEN,
 });
 
 const app = express();
